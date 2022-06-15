bff5c1e91b52e2fc30de01a70c79d6eed7c54240
MLCODEGitHub <MLCODEGitHub@gmail.com>
2022-06-15 19:02:30 +0530

    modifying app file

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
 
-app=Flask(__name__)
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
 
-@app.route("/",methods=['GET','POST'])
-def index():
-    return "CI CD pipeline has been established."
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
