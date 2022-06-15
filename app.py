e6be2f115591cc07380bcbde797ddea01f96e4f5
MLCODEGitHub <MLCODEGitHub@gmail.com>
2022-06-15 13:54:24 +0530

    adding app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from crypt import methods
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl','rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -12,13 +11,17 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    features = [int(each) for each in request.form.values()]
-    final_features = [np.array(features)]
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
-    output = np.round(prediction[0],2)[0]
-    
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))   
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+
 
 if __name__ == "__main__":
-    app.run(debug=True)    
+    app.run(debug=True)
