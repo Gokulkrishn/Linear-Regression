fb594bbcab30353d9b96b1b0f8634fada43a2c49
Gokulkrishn <K.Gokulkrishn@gmail.com>
2022-06-22 18:09:56 +0530

    Adding housing module

---
+++ setup.py
@@ -0,0 +1,24 @@
+import imp
+from pytz import VERSION
+from setuptools import setup
+from typing import List
+
+def get_requirements_list()->List[str]:
+    with open('requirements.txt') as f:
+        return f.readlines()
+
+PROJECT_NAME="housing-predictor"
+VERSION="0.0.1"
+AUTHOR="Gokul"
+DESCRIPTION="This is a housing price predictor project"
+PACKAGES=["housing"]
+
+setup(
+    name=PROJECT_NAME,
+    version=VERSION,
+    author=AUTHOR,
+    description=DESCRIPTION,
+    packages=PACKAGES,
+    install_requires=get_requirements_list()
+
+)
