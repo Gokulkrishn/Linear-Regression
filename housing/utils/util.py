bfa1db2e3eccfb0621a5664e96d70a490c07c606
Gokulkrishn <K.Gokulkrishn@gmail.com>
2022-07-14 23:04:56 +0530

    adding other files

---
+++ housing/utils/util.py
@@ -0,0 +1,10 @@
+import yaml
+import os,sys
+from housing.exception import HousingException
+
+def read_yaml_path(file_path:str) -> dict:
+    try:
+        with open(file_path, 'rb') as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise HousingException(e,sys) from e
