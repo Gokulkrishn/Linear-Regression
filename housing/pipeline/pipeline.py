bcaad72d072cf0fe90a383fd3f1871b8b4963aa9
Gokulkrishn <K.Gokulkrishn@gmail.com>
2022-07-15 18:26:21 +0530

    updated

--- housing/pipeline/pipeline.py
+++ housing/pipeline/pipeline.py
@@ -41,6 +41,7 @@ class Pipeline:
     def run_pipeline(self):
         try:
             data_ingestion_artifact = self.start_data_ingestion()
+            loggin.info("pipeline for data ingestion completed")
         except Exception as e:
             raise HousingException(e,sys) from e
     
